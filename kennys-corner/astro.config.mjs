bbba2d01eada873ec9aeb31070cd5c558e05e0a6
Kenneth Hoff <kenneth.98@hotmail.com>
2025-02-14 16:04:20 +0100

    Add Sitemap

--- kennys-corner/astro.config.mjs
+++ kennys-corner/astro.config.mjs
@@ -3,9 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+        site: 'http://localhost:4321',
+        vite: {
+                plugins: [tailwindcss()]
+        },
+
+        integrations: [sitemap()]
 });
