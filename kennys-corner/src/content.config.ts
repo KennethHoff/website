fa2562fa963c553902ffbdb55bf57bb97336a3d7
Kenneth Hoff <kenneth.98@hotmail.com>
2025-02-14 15:37:41 +0100

    Create a newsletter

---
+++ kennys-corner/src/content.config.ts
@@ -0,0 +1,13 @@
+import { glob } from "astro/loaders";
+import { defineCollection, z } from "astro:content";
+
+const newsletter = defineCollection({
+        loader: glob({ pattern: "**/*.md", base: "./src/newsletter" }),
+        schema: z.object({
+                title: z.string(),
+                author: z.string(),
+                description: z.string().optional(),
+        }),
+})
+
+export const collections = { newsletter };
