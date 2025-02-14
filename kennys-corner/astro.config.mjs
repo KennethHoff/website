a89afdbb266c047697931cbf7141cb4955ec7465
Kenneth Hoff <kenneth.98@hotmail.com>
2025-02-14 16:18:29 +0100

    Remove whitespace from config

--- kennys-corner/astro.config.mjs
+++ kennys-corner/astro.config.mjs
@@ -1,20 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import sitemap from '@astrojs/sitemap';
-
 import vercel from '@astrojs/vercel';
 
-// https://astro.build/config
 export default defineConfig({
         site: 'http://localhost:4321',
-
         vite: {
                 plugins: [tailwindcss()]
         },
-
         integrations: [sitemap()],
         adapter: vercel(),
         output: 'server'
