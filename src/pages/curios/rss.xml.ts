333fefaf77d88df4fff934c420bb9ef801b99f85
Kenneth Hoff <kenneth.98@hotmail.com>
2025-04-08 21:42:15 +0200

    Various improvements to the RSS feed.

--- src/pages/curios/rss.xml.ts
+++ src/pages/curios/rss.xml.ts
@@ -3,15 +3,17 @@ import type { APIContext } from 'astro';
 import { getCollection } from 'astro:content';
 
 export async function GET(context: APIContext) {
-    const curios = await getCollection('curios');
-    return rss({
-        title: "Kenny's Corner",
-        description: "Kenny's Corner's Curios",
-        site: context.site!,
-        items: curios.map(curio => ({
-            title: curio.data.title,
-            pubDate: curio.data.created,
-            description: curio.data.description,
-        }))
-    });
+        const curios = await getCollection('curios');
+        return rss({
+                title: "Kenny's Curios",
+                description: "A random assortment of things I found interresting at some point. Mostly web-dev.",
+                site: new URL("curios", context.site),
+                items: curios.map(curio => ({
+                        title: curio.data.title,
+                        pubDate: curio.data.created,
+                        description: curio.data.description,
+                        link: new URL(`curios/${curio.id}`, context.site).toString(),
+                })),
+                trailingSlash: false,
+        });
 }
