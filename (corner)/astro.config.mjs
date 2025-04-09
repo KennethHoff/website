d96ad3616d498d3ab18cd0e040c7d353325a1d1d
Kenneth Hoff <kenneth.98@hotmail.com>
2025-04-09 21:20:46 +0200

    Use a distinct port for each app

--- (corner)/astro.config.mjs
+++ (corner)/astro.config.mjs
@@ -8,21 +8,24 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 const siteUrl =
-        // If Production, use production URL
-        process.env.VERCEL_ENV == "production"
-                ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-                : // Otherwise, if you have a VERCEL_URL, use it
-                process.env.VERCEL_URL
-                        ? `https://${process.env.VERCEL_URL}`
-                        : // Otherwise, just use localhost.
-                        "http://localhost:4321";
+    // If Production, use production URL
+    process.env.VERCEL_ENV == "production"
+        ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+        : // Otherwise, if you have a VERCEL_URL, use it
+        process.env.VERCEL_URL
+            ? `https://${process.env.VERCEL_URL}`
+            : // Otherwise, just use localhost.
+            "http://localhost:4322";
 
 export default defineConfig({
-        site: siteUrl,
-        vite: {
-                plugins: [tailwindcss()],
-        },
-        integrations: [sitemap(), react()],
-        adapter: vercel(),
-        trailingSlash: "never",
+    site: siteUrl,
+    server: {
+        port: 4322
+    },
+    vite: {
+        plugins: [tailwindcss()],
+    },
+    integrations: [sitemap(), react()],
+    adapter: vercel(),
+    trailingSlash: "never",
 });
