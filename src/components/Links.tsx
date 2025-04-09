8c6306cfce5e7db46dc7253846d7d559a5ac60d7
Kenneth Hoff <kenneth.hoff@oxx.no>
2025-04-09 15:16:35 +0200

    Add link to GitHub repository

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -1,4 +1,4 @@
-import { FileUser, RssIcon } from "lucide-react";
+import { FileUser, GithubIcon, RssIcon } from "lucide-react";
 import { TooltipProvider, TooltipTrigger, Tooltip, TooltipContent } from "./ui/tooltip";
 
 export function ResumeLink() {
@@ -26,6 +26,21 @@ export function CuriosRssFeedLink() {
                     </a>
                 </TooltipTrigger>
                 <TooltipContent>RSS Feed for Kenny's Curios</TooltipContent>
+            </Tooltip>
+        </TooltipProvider>
+    );
+}
+
+export function GithubLink() {
+    return (
+        <TooltipProvider>
+            <Tooltip>
+                <TooltipTrigger asChild>
+                    <a href="https://github.com/kennethhoff/kennyscorner" aria-label="The GitHub repository for this website">
+                        <GithubIcon aria-hidden />
+                    </a>
+                </TooltipTrigger>
+                <TooltipContent>The GitHub repository for this website</TooltipContent>
             </Tooltip>
         </TooltipProvider>
     );
