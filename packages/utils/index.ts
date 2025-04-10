83ceabc0f21f16862b47d24dea1980de046232fb
Kenneth Hoff <kenneth.98@hotmail.com>
2025-04-10 23:11:30 +0200

    move `defineStructuredData` into `@lib/utils`

--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -1,11 +1,14 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import type { Thing, WithContext } from "schema-dts";
 
 export function cn(...inputs: ClassValue[]) {
-    return twMerge(clsx(inputs));
+        return twMerge(clsx(inputs));
 }
 
-export function print() {
-	console.log("printing");
+export function defineStructuredData<T extends Thing>(
+        thing: WithContext<T>,
+): WithContext<T> {
+        return thing;
 }
 
