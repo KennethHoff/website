a36ea18da87234e98bf0f35b78107278a025b913
Kenneth Hoff <kenneth.98@hotmail.com>
2025-04-11 08:58:13 +0200

    Revert "Create @lib/utils"
    This reverts commit 841497d56778270eea760b4736451b262b3f5a47.

--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -1,11 +1,3 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs: ClassValue[]) {
-    return twMerge(clsx(inputs));
-}
-
 export function print() {
 	console.log("printing");
 }
-
