b8e9735dcec0e02eaaa33afa794ba7db6bdc2938
Kenneth Hoff <kenneth.hoff@oxx.no>
2025-06-05 10:02:57 +0200

    Create new blog on the new Temporal API

--- (corner)/src/pages/rss.xml.ts
+++ (corner)/src/pages/rss.xml.ts
@@ -4,12 +4,13 @@ import type { APIContext } from "astro";
 
 export async function GET(context: APIContext) {
     const articles = await getCollection("articles");
+    const sortedArticles = articles.toSorted((a, b) => b.data.created.getTime() - a.data.created.getTime());
     return rss({
         title: "Kenny's Corner",
         description:
             "A random assortment of things I found interresting at some point. Mostly web-dev.",
         site: new URL(context.site!),
-        items: articles.map((article) => ({
+        items: sortedArticles.map((article) => ({
             title: article.data.title,
             pubDate: article.data.created,
             description: article.data.description,
